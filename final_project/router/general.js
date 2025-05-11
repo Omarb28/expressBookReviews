8978017352d3d590980c0ecb985e74a394d1ce3e
Omarb28 <omarb28@gmail.com>
2025-05-11 03:14:56 -0400

    Implement endpoints for retrieving books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,28 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_books = {}
+
+  for (const book_key in books) {
+    const book = books[book_key];
+    if (book.author === author) {
+      filtered_books[book_key] = book;
+    }
+  }
+
+  res.send(filtered_books);
 });
 
 // Get all books based on title
